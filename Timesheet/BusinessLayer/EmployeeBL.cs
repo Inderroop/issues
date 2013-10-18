0673d65656f7d856c0b6cffa92546b356c184286
Inderroop <Inderroop_nagra@yahoo.com>
2013-10-18 14:54:23 -0400

    Business Layer Update
    Business Layer Update

---
+++ Timesheet/BusinessLayer/EmployeeBL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.Diagnostics;
+
+namespace BusinessLayer.Employee
+{
+    public class EmployeeBL
+    {
+        public void AddNewEmployee()
+        {
+            Employee.LoginDate = DateTime.Now;
+            Employee.LoginTime = DateTime.Now;
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+    }
+    
